475f3d1607e776fa8905807ef6ca801de7e41eff
imp908 <imp908@yandex.ru>
2018-04-20 14:28:05 +0300

    new Quiz model NodeCollection

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { App7Module } from './app/app7/app7.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(App7Module)
   .catch(err => console.log(err));
